39848f9bdd29cd939a4440cd0e03ac1ad9677e6e
Artur <artur090806@yandex.ru>
2023-10-23 16:49:35 +0300

    test commit

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ const upload = multer({ storage })
 app.use(express.json())
 app.use(cors())
 app.use('/uploads', express.static('uploads'))
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+	next()
+})
 
 app.post(
 	'/auth/login',
