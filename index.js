1e66f5c053a6ca109ef80563d591c18fd533a69c
Artur <artur090806@yandex.ru>
2023-10-25 15:36:37 +0300

    test 9

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 const corsOptions = {
-	origin: ['dive-sea-frontend.vercel.app'],
+	origin: ['https://dive-sea-frontend.vercel.app'],
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 	credentials: true,
